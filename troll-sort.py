eaf022a96023a3eb512dabe96444d283bfa96570
Dan Pevny <dgp28@njit.edu>
2020-12-02 18:43:47 -0500

    Pseudocode algorithm selection

--- troll-sort.py
+++ troll-sort.py
@@ -1,6 +1,21 @@
+# import random
+
 def trollsort(ints):
     for elem in ints:
         print(elem)
+
+    '''
+    Randomly select a sorting algorithm
+    algSelection = random.randint(0, 1)
+
+    if algSelection == 0:
+        print('Using bubble sort')
+        bubble-sort(ints)
+    if algSelection == 1:
+        print('Using merge sort')
+        merge-sort(ints)
+
+    '''
 
 ints = [1, 2]
 
