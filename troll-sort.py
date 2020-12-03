b42813a1f6a6b7bbabe238eff6bcbbaf873d11cb
Dan Pevny <dgp28@njit.edu>
2020-12-02 19:32:03 -0500

    Implement merge_sort

--- troll-sort.py
+++ troll-sort.py
@@ -5,13 +5,14 @@ def troll_sort(ints):
     algSelection = random.randint(0, 1)
 
     if algSelection == 0:
-        # print('Using bubble sort')
+        print('Using bubble sort')
         bubble_sort(ints)
     if algSelection == 1:
-        # print('Using merge sort')
+        print('Using merge sort')
         merge_sort(ints)
 
 def bubble_sort(ints):
+    # print('Using bubble sort')
     # Modified from https://www.geeksforgeeks.org/bubble-sort/
     n = len(ints)
 
@@ -28,10 +29,53 @@ def bubble_sort(ints):
                 ints[j], ints[j+1] = ints[j+1], ints[j]
 
 def merge_sort(ints):
-    print('Using merge sort')
+    # print('Using merge sort')
 
-ints = [1, 2]
-ints = [64, 34, 25, 12, 22, 11, 90]
+    # Modified from https://www.geeksforgeeks.org/merge-sort/
+    if len(ints) > 1:
+
+         # Finding the mid of the array
+        mid = len(ints)//2
+
+        # Dividing the array elements
+        L = ints[:mid]
+
+        # into 2 halves
+        R = ints[mid:]
+
+        # Sorting the first half
+        merge_sort(L)
+
+        # Sorting the second half
+        merge_sort(R)
+
+        i = j = k = 0
+
+        # Copy data to temp arrays L[] and R[]
+        while i < len(L) and j < len(R):
+            if L[i] < R[j]:
+                ints[k] = L[i]
+                i += 1
+            else:
+                ints[k] = R[j]
+                j += 1
+            k += 1
+
+        # Checking if any element was left
+        while i < len(L):
+            ints[k] = L[i]
+            i += 1
+            k += 1
+
+        while j < len(R):
+            ints[k] = R[j]
+            j += 1
+            k += 1
+
+#ints = [1, 2]
+#ints = [64, 34, 25, 12, 22, 11, 90]
+
+ints = [12, 11, 13, 5, 6, 7]
 
 troll_sort(ints)
 
