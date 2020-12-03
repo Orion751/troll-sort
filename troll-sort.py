4ee98bd5800dc6ced18a1c34e254e0523e9ce25f
Dan Pevny <dgp28@njit.edu>
2020-12-02 19:10:43 -0500

    Implement bubble_sort

--- troll-sort.py
+++ troll-sort.py
@@ -1,28 +1,39 @@
-# import random
+import random
 
 def trollsort(ints):
-    for elem in ints:
-        print(elem)
-
-    '''
-    Randomly select a sorting algorithm
+    # Randomly select a sorting algorithm
     algSelection = random.randint(0, 1)
 
     if algSelection == 0:
-        print('Using bubble sort')
+        # print('Using bubble sort')
         bubble_sort(ints)
     if algSelection == 1:
-        print('Using merge sort')
+        # print('Using merge sort')
         merge_sort(ints)
 
-    '''
-
 def bubble_sort(ints):
-    print('Using bubble sort')
+    # Modified from https://www.geeksforgeeks.org/bubble-sort/
+    n = len(ints)
+
+    # Traverse through all array elements
+    for i in range(n):
+
+        # Last i elements are already in place
+        for j in range(0, n-i-1):
+
+            # traverse the array from 0 to n-i-1
+            # Swap if the element found is greater
+            # than the next element
+            if ints[j] > ints[j+1] :
+                ints[j], ints[j+1] = ints[j+1], ints[j]
 
 def merge_sort(ints):
     print('Using merge sort')
 
 ints = [1, 2]
+ints = [64, 34, 25, 12, 22, 11, 90]
 
 trollsort(ints)
+
+for elem in ints:
+    print(elem)
